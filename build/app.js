e5ff59d31824bdf18a98ec3dfe8e4dbf29ba8609
Diego Anestor Coutinho <diegoacoutinho1224@gmail.com>
2022-05-24 21:36:20 -0300

    Added suporte ao roteamento de arquivos html

--- build/app.js
+++ build/app.js
@@ -1,2 +1,17 @@
 "use strict";
-console.log('Hello World!');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const path_1 = __importDefault(require("path"));
+const app = (0, express_1.default)();
+app.get('/', (req, res) => {
+    res.sendFile(path_1.default.join(__dirname, '..', 'public', 'index.html'));
+});
+app.get('/login', (req, res) => {
+    res.sendFile(path_1.default.join(__dirname, '..', 'public', 'login.html'));
+});
+app.listen(3000, () => {
+    console.log('Server started on port 3000');
+});
--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,21 @@
-// Printa uma mensagem de testes
-console.log('Hello World!');
+// Importa dependências - Express, path, body-parser
+import bodyParser from 'body-parser';
+import express from 'express';
+import path from 'path';
+
+const app = express();
+
+// Na rota raiz, retorna index.html
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
+
+// Na rota /login, retorna login.html
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'login.html'));
+});
+
+// Adiciona o servidor na porta 3000
+app.listen(3000, () => {
+    console.log('Server started on port 3000');
+});
