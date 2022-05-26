1d222678982628973cb8d1a3d35d6dfbdef7785c
Diego Anestor Coutinho <diegoacoutinho1224@gmail.com>
2022-05-26 14:29:59 -0300

    Added página "plano"

--- build/app.js
+++ build/app.js
@@ -33,6 +33,7 @@ const express_1 = __importDefault(require("express"));
 const path_1 = __importDefault(require("path"));
 const app = (0, express_1.default)();
 app.use(body_parser_1.default.urlencoded({ extended: false }));
+app.use(express_1.default.static(path_1.default.join(__dirname, '..', 'public')));
 app.get('/', (req, res) => {
     res.sendFile(path_1.default.join(__dirname, '..', 'public', 'index.html'));
 });
